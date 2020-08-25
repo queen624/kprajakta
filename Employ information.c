e1616b51a8a30c8fccab585d678a9ae07efcdc4b
queen624 <70107967+queen624@users.noreply.github.com>
2020-08-25 22:46:13 +0530

    Employ information.c
    This print the information of the employee.

---
+++ Employ information.c
@@ -0,0 +1,17 @@
+#include<iostream.h>
+Struct employee
+{
+Char name[30];
+Int empage;
+Int phone no.;
+Float salary;
+};
+Int main()
+Struct emplyee emp;
+Printf("\n enter details\n");
+Printf("name ?:");   gets(emp.name);
+Printf("age ?:");     scanf("%d",&emp.empage);
+Printf("phone no.?:");  scanf("%d",&emp.empphone no.);
+Printf("salary ?:");    scanf("%f",&emp.salary);
+Return 0;
+}
